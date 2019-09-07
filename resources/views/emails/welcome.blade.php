adf90eb8f239700a0a54a9225d22d6dd4a96421a
Amy <ampyaephyonaing@gmail.com>
2019-09-07 20:15:25 +0630

    markdown

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -3,8 +3,10 @@
 
 Welcome!!!!
 
-@component('mail::button', ['url' => ''])
-Button Text
+Queue Test!!
+
+@component('mail::button', ['url' => 'http://ec2-13-229-129-10.ap-southeast-1.compute.amazonaws.com/home'])
+Click Here
 @endcomponent
 
 Thanks,<br>
