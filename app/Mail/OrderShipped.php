ac8c7bec3456042b88ee3f4ddf1dc593ad1fca62
Amy <ampyaephyonaing@gmail.com>
2019-09-07 17:52:39 +0630

    Queue

--- app/Mail/OrderShipped.php
+++ app/Mail/OrderShipped.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class RegisterMail extends Mailable
+class OrderShipped extends Mailable
 {
     use Queueable, SerializesModels;
 
@@ -28,8 +28,6 @@ class RegisterMail extends Mailable
      */
     public function build()
     {
-        $path = storage_path("app/resumes/cv.pdf");
-
-        return $this->markdown('emails.welcome');
+        return $this->markdown('emails.orders.shipped');
     }
 }
